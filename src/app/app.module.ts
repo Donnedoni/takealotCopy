d74064a3010b7ce4bc6750df5a1383d8f1ef2569
DA MAC M1 150 <damacm1150@DAs-MacBook-Air.local>
2023-04-21 12:46:31 +0200

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    SliderComponent,
+    ProductCardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
